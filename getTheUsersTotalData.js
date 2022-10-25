440979387f980dc5a3cce40202cdcbd774c8201d
maximas033 <101352756+maximas033@users.noreply.github.com>
2022-10-25 12:11:57 -0700

    Add files via upload

--- getTheUsersTotalData.js
+++ getTheUsersTotalData.js
@@ -1,13 +1,17 @@
 // from the real-time database refrence users
 // then refrence users/totalTime" + name
 function getTotalTime(){
-    var name = ["David", "Maxim", "Vadim", "Vitaliy", "Dan", "Pasha", "Gena", "Slavic", "Roman", "Edward", "Dima", "Viktor", "Paul", "Test"];
-    for(var i = 0; i < name.length; i++){
-        firebase.database().ref("users/totalTime" + name[i]).on("value", function(snapshot){
-            // display the total time that the user has worked inside console
-            console.log(snapshot.val());
-            // create a table that will display the name of the user and the total hours and minutes that the user has worked
-            // appened the table to the div with the id of workedHoursW
+    // get the names of the people from the firebase database under SCCSECURITYPEOPLE
+    var SCCSECURITYPEOPLE = firebase.database().ref().child("SCCSECURITYPEOPLE");
+    // get the total time of time worked for each person
+    SCCSECURITYPEOPLE.on("value", function(snapshot) {
+        snapshot.forEach(function(data) {
+            var name = data.key;
+            var totalTime = firebase.database().ref().child("users/totalTime" + name);
+            totalTime.on("value", function(snapshot) {
+                var total = snapshot.val();
+               // create a table that will display the name of the user and the total hours and minutes that the user has worked
+//             // appened the table to the div with the id of workedHoursW
             var table = document.createElement("table");
             table.setAttribute("class", "table table-striped");
             var thead = document.createElement("thead");
@@ -40,6 +44,51 @@ function getTotalTime(){
             tbody.appendChild(tr);
             table.appendChild(tbody);
             document.getElementById("workedHoursW").appendChild(table);
+            });
         });
-    }
-} 
+    });
+}
+
+
+//     firebase.database().ref("users/totalTime" + SCCSECURITYPEOPLE).once("value", function(snapshot){
+//     // name of the user
+//     for(var i = 0; i < SCCSECURITYPEOPLE.length; i++){
+//         firebase.database().ref("users/totalTime" + SCCSECURITYPEOPLE[i]).once("value", function(snapshot){
+//             // create a table that will display the name of the user and the total hours and minutes that the user has worked
+//             // appened the table to the div with the id of workedHoursW
+//             var table = document.createElement("table");
+//             table.setAttribute("class", "table table-striped");
+//             var thead = document.createElement("thead");
+//             var tr = document.createElement("tr");
+//             var th = document.createElement("th");
+
+//             th.innerHTML = "Name";
+//             tr.appendChild(th);
+//             th = document.createElement("th");
+//             th.innerHTML = "Hours";
+//             tr.appendChild(th);
+//             th = document.createElement("th");
+//             th.innerHTML = "Minutes";
+//             tr.appendChild(th);
+//             thead.appendChild(tr);
+//             table.appendChild(thead);
+//             var tbody = document.createElement("tbody");
+//             tr = document.createElement("tr");
+//             var td = document.createElement("td");
+//             td.innerHTML = snapshot.val().name;
+//             tr.appendChild(td);
+//             td = document.createElement("td");
+//             td.innerHTML = snapshot.val().totalHour;
+//             tr.appendChild(td);
+//             td = document.createElement("td");
+//             td.innerHTML = snapshot.val().totalMinute;
+//             // remove the line before the minute
+//             td.innerHTML = td.innerHTML.replace("-", "");
+//             tr.appendChild(td);
+//             tbody.appendChild(tr);
+//             table.appendChild(tbody);
+//             document.getElementById("workedHoursW").appendChild(table);
+//         });
+//     }
+//     });
+// }
